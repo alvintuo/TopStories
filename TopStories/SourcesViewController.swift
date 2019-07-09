33d1ef6ce72fbc0071b5acd65319c12f981f33f6
Alvin Tuo <atuo@wyomingseminary.org>
2019-07-09 10:17:01 -0500

    Version 0.1 pre-Developer Beta - Made Changes to Storyboard

--- TopStories/SourcesViewController.swift
+++ TopStories/SourcesViewController.swift
@@ -8,11 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SourcesViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
