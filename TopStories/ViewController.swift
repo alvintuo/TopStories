a4dfb3c3c80dfea2564a6a75c6523964221ace4d
Alvin Tuo <atuo@wyomingseminary.org>
2019-07-09 10:07:48 -0500

    Initial Commit

---
+++ TopStories/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TopStories
+//
+//  Created by Alvin Tuo on 7/9/19.
+//  Copyright © 2019 SWIFT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
